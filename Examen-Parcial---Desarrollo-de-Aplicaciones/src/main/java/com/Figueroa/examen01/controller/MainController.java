fda52b4037bf6f85f9338d2ce609aa6f9712ce93
YissAFC <jesus12del24@gmail.com>
2024-10-24 15:52:00 -0500

    Add files via upload

---
+++ Examen-Parcial---Desarrollo-de-Aplicaciones/src/main/java/com/Figueroa/examen01/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.Figueroa.examen01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/infracciones")
+    public String infracciones() {
+        return "infracciones";
+    }
+}
---
+++ Examen-Parcial---Desarrollo-de-Aplicaciones/src/main/java/com/Figueroa/examen01/converter/InfraccionConverter.java
@@ -0,0 +1,39 @@
+package com.Figueroa.examen01.converter;
+import org.springframework.stereotype.Component;
+
+import com.Figueroa.examen01.dto.InfraccionDto;
+import com.Figueroa.examen01.entity.Infraccion;
+
+
+
+@Component
+public class InfraccionConverter extends AbstractConverter<Infraccion,InfraccionDto> { 
+    @Override
+    public InfraccionDto fromEntity(Infraccion entity) {
+        if (entity == null) return null;
+
+        return InfraccionDto.builder()
+                .id(entity.getId())
+                .dni(entity.getDni())
+                .placa(entity.getPlaca())
+                .descripcion(entity.getDescripcion())
+                .infracción(entity.getInfracción())
+                .fecha(entity.getFecha())
+                .build();
+    }
+
+    @Override
+    public Infraccion fromDTO(InfraccionDto dto) {
+        if (dto == null) return null;
+
+        return Infraccion.builder()
+                .id(dto.getId())
+                .dni(dto.getDni())
+                .placa(dto.getPlaca())
+                .descripcion(dto.getDescripcion())
+                .infracción(dto.getInfracción())
+                .fecha(dto.getFecha())
+                .build();
+    }
+
+}
